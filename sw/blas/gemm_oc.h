71070e2d2dc37743e89464f7da59b89a6e2d86dd
Roger Barton <rbarton@student.ethz.ch>
2024-01-03 16:17:38 +0100

    Use dma tile functions in gemm

--- sw/blas/gemm_oc.h
+++ sw/blas/gemm_oc.h
@@ -15,6 +15,7 @@
  * \param dir Sets the direction of traversal. True: loop starts at begin.
  */
 #define FOR_EACH(i, begin, end, stride, dir)                                   \
+  dir = !dir;                                                                  \
   const int first = dir ? begin : end - 1;                                     \
   const int last = dir ? end : begin;                                          \
   for (i = first; dir ? i < last : i >= last; dir ? i += stride : i -= stride)
@@ -43,14 +44,20 @@ inline void gemm_oc_baseline(double alpha, double beta,
                              uint32_t m, uint32_t n, uint32_t k,
                              void* A, void* B, void* C,
                              uint32_t lda, uint32_t ldb, uint32_t ldc) {
+    /**
+    * Problem is double buffered in L1. The buffer that is used is toggled at each iteration.
+    * The DMA cores are one index step ahead so they load the data in advance into the buffer that will be used.
+    *
+    */
+
     // Setup layout for TCDM L1
     // For double buffering l1 is a size 2 array
-    TcdmLayout* const l1    = (TcdmLayout*) snrt_l1_allocator()->base;
-    bool l1ComputeBuffer_AB = false;
-    bool l1ComputeBuffer_C  = false;
+    TcdmLayout* const l1 = (TcdmLayout*) snrt_l1_allocator()->next;
+    bool l1Id_AB         = false;
+    bool l1Id_C          = false;
 
     // Initialize indices
-    const int I  = 8, J  = 8, K = 4;
+    const int I  = m, J  = n, K = k;
     const int PI = 2, PJ = 2;
     const int P  = PI * PJ;
 
@@ -58,63 +65,62 @@ inline void gemm_oc_baseline(double alpha, double beta,
     const int pi = p / PI;
     const int pj = p % PI;
 
-    int ib     = 0, jb       = 0, kb        = 0;
-    bool i_dir = true, j_dir = false, k_dir = false;
+    int ib     = 0, jb        = 0, kb        = 0;
+    bool i_dir = false, j_dir = false, k_dir = false;
 
     if (snrt_is_compute_core()) {
         snrt_cluster_hw_barrier(); // DMA core is one index ahead
     }
 
     FOR_EACH(ib, pi, I - PI + pi +1, PI, i_dir) {
-        j_dir = !j_dir;
         FOR_EACH(jb, pj, J - PJ + pj +1, PJ, j_dir) {
-            // TODO: l1 buffer is decided by loop iter, dma is a step ahead so no need to do different calculations
-            //       just toggle which one is used at each iter
-            auto* const l1_C = l1[l1ComputeBuffer_C].C;
+            const auto i = ib * l1_lda;
+            const auto j = jb * l1_ldb;
+
+            auto* const l1_C = l1[l1Id_C].C;
 
             // load next C
             snrt_dma_txid_t dma_tx_C = -1;
             if (snrt_is_dm_core()) {
-                const auto* const l3_C_block = C + ib * ldc + jb; // C[i][j]
-                auto* const l1_C_dma = l1[!l1ComputeBuffer_C].C;
-                dma_tx_C = snrt_dma_start_2d(l1_C_dma, l3_C_block, l1_ldc, l1_ldc, ldc, sizeof(l1->C) / l1_ldc);
+                dma_tx_C = snrt_dma_load_2d_tile(l1_C, c, ib, jb, l1_M, l1_N, ldc, FP64);
                 // TODO: implement piecewise transfer of C in inner loop
             }
-            k_dir = !k_dir;
+
             FOR_EACH(kb, 0, K, 1, k_dir) {
+                auto* const l1_A = l1[l1Id_AB].A;
+                auto* const l1_B = l1[l1Id_AB].B;
+
                 // load next A, B
                 if (snrt_is_dm_core()) {
-                    const auto* const l3_A_block = A + ib * lda + kb; // A[i][k]
-                    const auto* const l3_B_block = B + kb * ldb + jb; // B[k][j]
+                    snrt_dma_load_2d_tile(l1_A, a, ib, kb, l1_M, l1_K, lda, FP64);
+                    snrt_dma_load_2d_tile(l1_B, b, kb, jb, l1_K, l1_N, ldb, FP64);
 
-                    // TODO: transfer strided l3 block into a contiguous l1 block, use snrt_dma_start_2d
-                    auto* const l1_A_dma = l1[!l1ComputeBuffer_AB].A;
-                    auto* const l1_B_dma = l1[!l1ComputeBuffer_AB].B;
-
-                    // TODO: use tile method from git:dnn/verification, just define above
-                    const auto dma_tx_A = snrt_dma_start_2d(l1_A_dma, l3_A_block, l1_lda, l1_lda, lda,
-                                                            sizeof(l1->A) / l1_lda);
-                    const auto dma_tx_B = snrt_dma_start_2d(l1_B_dma, l3_B_block, l1_ldb, l1_ldb, ldb,
-                                                            sizeof(l1->B) / l1_ldb);
-                    snrt_dma_wait(dma_tx_A);
-                    snrt_dma_wait(dma_tx_B);
+                    snrt_dma_wait_all();
                 } else {
-                    auto* const l1_A = l1[l1ComputeBuffer_AB].A;
-                    auto* const l1_B = l1[l1ComputeBuffer_AB].B;
-                    // solve block already in l1
+                    // solve block already in l1, parallelize inside each cluster
                     gemm(FP64, 0, true, false, false,
                          m, n, k, alpha,
                          l1_A, l1_lda, l1_B, l1_ldb, beta, l1_C, l1_ldc);
                 }
 
-                l1ComputeBuffer_AB = !l1ComputeBuffer_AB; // switch buffers
+                l1Id_AB = !l1Id_AB; // switch buffers
                 snrt_cluster_hw_barrier();
             }
+
             if (snrt_is_dm_core()) {
                 snrt_dma_wait(dma_tx_C);
             }
-            l1ComputeBuffer_C = !l1ComputeBuffer_C; // switch buffers
+
+            l1Id_C = !l1Id_C; // switch buffers
             snrt_cluster_hw_barrier();
+
+            if(snrt_is_dm_core()) { // store C
+                dma_tx_C = snrt_dma_store_2d_tile(c, l1_C,
+                                       ib, jb,
+                                       l1_M, l1_N, ldc, FP64);
+
+                snrt_dma_wait(dma_tx_C);
+            }
         }
     }
 
