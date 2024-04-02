22f17632a2b222adae24c6dda32246ce4e82e9da
Roger Barton <rbarton@student.ethz.ch>
2024-04-02 17:02:20 +0200

    singlebuffer and streaming buffer tiling

--- sw/blas/gemm/src/gemm.h
+++ sw/blas/gemm/src/gemm.h
@@ -22,7 +22,7 @@
 #undef FLOAT_T
 #undef METHOD
 
-// -- Baseline
+// -- 1dpipe
 #define METHOD 1dpipe
 #define FLOAT_T fp64
 #include "gemm_tpl.h"
@@ -43,6 +43,44 @@
 
 // -- Baseline
 #define METHOD baseline
+#define FLOAT_T fp64
+#include "gemm_tpl.h"
+#undef FLOAT_T
+
+#define FLOAT_T fp32
+#include "gemm_tpl.h"
+#undef FLOAT_T
+
+#define FLOAT_T fp16
+#include "gemm_tpl.h"
+#undef FLOAT_T
+
+#define FLOAT_T fp8
+#include "gemm_tpl.h"
+#undef FLOAT_T
+#undef METHOD
+
+// -- Single Buffer
+#define METHOD singlebuffer
+#define FLOAT_T fp64
+#include "gemm_tpl.h"
+#undef FLOAT_T
+
+#define FLOAT_T fp32
+#include "gemm_tpl.h"
+#undef FLOAT_T
+
+#define FLOAT_T fp16
+#include "gemm_tpl.h"
+#undef FLOAT_T
+
+#define FLOAT_T fp8
+#include "gemm_tpl.h"
+#undef FLOAT_T
+#undef METHOD
+
+// -- Streaming Buffer
+#define METHOD streambuffer
 #define FLOAT_T fp64
 #include "gemm_tpl.h"
 #undef FLOAT_T
---
+++ sw/blas/gemm/src/gemm_tiling_singlebuffer_tpl.h
@@ -0,0 +1,135 @@
+#ifndef FLOAT_T 
+#error "Define FLOAT_T to use this template."
+#endif
+
+#ifndef IS_DM_CORE
+#error "Define IS_DM_CORE to use this template."
+#endif
+
+#include "gemm_kernel.h"
+
+__attribute__((flatten)) // Force inline called functions
+void SNBLAS_GEMM_TILING(singlebuffer, FLOAT_T, IS_DM_CORE) (const SnblasGemmInfo info, const SNBLAS_GEMM_ARGS(FLOAT_T) args, const SnblasGemmImpl impl) {
+    
+    /**
+     * Problem is double buffered in L1. The buffer that is used is toggled at
+     * each iteration. The DMA cores are one index step ahead so they load the
+     * data in advance into the buffer that will be used.
+     */
+
+    typedef SNBLAS_GEMM_TCDM(FLOAT_T) TcdmLayout;
+    typedef SnblasGemmInfo GemmInfo;
+    typedef SNBLAS_GEMM_ARGS(FLOAT_T) GemmArgs;
+
+    if (impl.bench) snrt_mcycle();
+
+    const uint32_t M   = info.M;
+    const uint32_t N   = info.N;
+    const uint32_t K   = info.K;
+    const uint32_t lda = info.ta ? info.M : info.K;
+    const uint32_t ldb = info.tb ? info.K : info.N;
+    const uint32_t ldc = info.tc ? info.M : info.N;
+    const uint32_t ta  = info.ta;
+    const uint32_t tb  = info.tb;
+
+    const FLOAT_T* const A = args.A;
+    const FLOAT_T* const B = args.B;
+          FLOAT_T* const C = args.C;
+    const FLOAT_T alpha    = args.alpha;
+    const FLOAT_T beta     = args.beta;
+
+    uint32_t p[3] = {0, 0, 0};
+    uint32_t P[3] = {0, 0, 0};
+    ocrt_thread_idx(p);
+    ocrt_compute_thread_num(P);
+
+    // Setup layout for TCDM L1
+    // For double buffering l1 is a size 2 array
+    TcdmLayout* l1 = (TcdmLayout*)snrt_l1_next();
+
+    // Initialize indices
+    const uint32_t PI = 1, PJ = 1;
+    const uint32_t pi = p[1] / PJ;
+    const uint32_t pj = p[1] % PJ;
+
+    int ib, jb, kb;
+
+    bool storeC = false;
+
+    GemmInfo tileInfo = {0};
+    tileInfo.M   = L1_M;
+    tileInfo.N   = L1_N;
+    tileInfo.K   = L1_K;
+    tileInfo.ta  = info.ta ^ TA_TILE;
+    tileInfo.tb  = info.tb ^ TB_TILE;
+    tileInfo.tc  = info.tc ^ TC_TILE; // TODO: implement transposed blocking
+    tileInfo.lda = tileInfo.ta ? tileInfo.M : tileInfo.K;
+    tileInfo.ldb = tileInfo.tb ? tileInfo.K : tileInfo.N;
+    tileInfo.ldc = tileInfo.tc ? tileInfo.M : tileInfo.N;
+
+    // create function ptr for dma loading
+    const snrt_dma_load_2d_tile_t load_tile_A = TA_TILE ? &snrt_dma_load_2d_tile_transpose : &snrt_dma_load_2d_tile;
+    const snrt_dma_load_2d_tile_t load_tile_B = TB_TILE ? &snrt_dma_load_2d_tile_transpose : &snrt_dma_load_2d_tile;
+    const snrt_dma_load_2d_tile_t load_tile_C = (args.beta == (FLOAT_T)0.0) ? &load_zero_tile : 
+                                                TC_TILE ? &snrt_dma_load_2d_tile_transpose : &snrt_dma_load_2d_tile;
+    const snrt_dma_load_2d_tile_t store_tile_C = TC_TILE ? &snrt_dma_store_2d_tile_transpose : &snrt_dma_store_2d_tile;
+    
+    if (!IS_DM_CORE) {
+        SNBLAS_GEMM_CLUSTER_KERNEL_INIT(FLOAT_T)(tileInfo, impl);
+    }
+    if (impl.bench) snrt_mcycle();
+
+    for(ib = pi; ib <  M / L1_M; ib += PI) {
+        for(jb = pj; jb < N / L1_N; jb += PJ) {
+            if (IS_DM_CORE) {
+                dump_ib(ib);
+                dump_jb(jb);
+                (*load_tile_C)(l1->C, (void*) C, ib, jb, L1_M, L1_N, ldc, FP64);
+            }
+
+            for(kb = 0; kb < K / L1_K; kb++) {
+
+                if (IS_DM_CORE) {
+                    dump_kb(kb);
+                    (*load_tile_A)(l1->A, (void*) A, info.ta ? kb : ib, 
+                                                    info.ta ? ib : kb, 
+                                                    info.ta ? L1_K : L1_M, 
+                                                    info.ta ? L1_M : L1_K, lda, FP64);
+                    (*load_tile_B)(l1->B, (void*) B, info.tb ? jb : kb, 
+                                                    info.tb ? kb : jb, 
+                                                    info.tb ? L1_N : L1_K, 
+                                                    info.tb ? L1_K : L1_N, ldb, FP64);
+                    snrt_dma_wait_all();
+                    snrt_cluster_hw_barrier(); // start compute
+                    snrt_cluster_hw_barrier(); // wait for end of compute
+                    if (impl.bench) snrt_mcycle();
+                } else {
+                    GemmArgs tileArgs = {0};
+                    tileArgs.A     = l1->A;
+                    tileArgs.B     = l1->B;
+                    tileArgs.C     = l1->C;
+                    tileArgs.alpha = alpha;
+                    tileArgs.beta  = 1; // always accumulate partial result, args.beta already applied by dma
+                    
+                    snrt_cluster_hw_barrier(); 
+                    SNBLAS_GEMM_CLUSTER_KERNEL_COMPUTE(FLOAT_T)(tileInfo, tileArgs, impl);
+                    snrt_cluster_hw_barrier(); 
+                }
+                
+                if (impl.bench) snrt_mcycle();
+            }
+
+            if (IS_DM_CORE) {
+                (*store_tile_C)(C, l1->C, ib, jb, L1_M, L1_N, ldc, FP64);
+            }
+        }
+    }
+
+    if (IS_DM_CORE) {
+        snrt_dma_wait_all();
+    } else {
+        SNBLAS_GEMM_CLUSTER_KERNEL_DEINIT(FLOAT_T)(tileInfo, impl);
+    }
+    
+    if (impl.bench) snrt_mcycle();
+}
